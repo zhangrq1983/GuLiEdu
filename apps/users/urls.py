cfff6f959955b9b3cd3ffc53720756991075f085
zhangrq <zhangrongianglo@163.com>
2020-05-26 23:00:30 +0800

    update

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,26 +1,45 @@
 """GuLiEdu URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
-from users.views import *
+from .views import user_register, UserLoginView, user_logout, user_active, user_forget, user_reset, user_info, \
+    user_changeimage, user_changeinfo, user_changeemail, user_resetemail, user_course, user_loveteacher, user_loveorg, \
+    user_lovecourse, user_message, user_deletemessage
 
 urlpatterns = [
-    url(r'user_register/$', user_register, name='user_register'),
-    url(r'user_login/$', user_login, name='user_login'),
-    url(r'user_logout/$', user_logout, name='user_logout'),
-    url(r'user_active/(\w+)/$', user_active, name='user_active'),
-    url(r'user_forget/$', user_forget, name='user_forget'),
+    url(r'^user_register/$', user_register, name='user_register'),
+    url(r'^user_login/$', UserLoginView.as_view(), name='user_login'),
+    url(r'^user_logout/$', user_logout, name='user_logout'),
+    url(r'^user_active/(\w+)/$', user_active, name='user_active'),
+
+    url(r'^user_forget/$', user_forget, name='user_forget'),
     url(r'^user_reset/(\w+)/$', user_reset, name='user_reset'),
+
+    url(r'^user_info/$', user_info, name='user_info'),
+
+    url(r'^user_changeimage/$', user_changeimage, name='user_changeimage'),
+    url(r'^user_changeinfo/$', user_changeinfo, name='user_changeinfo'),
+    url(r'^user_changeemail/$', user_changeemail, name='user_changeemail'),
+    url(r'^user_resetemail/$', user_resetemail, name='user_resetemail'),
+
+    url(r'^user_course/$', user_course, name='user_course'),
+    url(r'^user_loveorg/$', user_loveorg, name='user_loveorg'),
+    url(r'^user_loveteacher/$', user_loveteacher, name='user_loveteacher'),
+    url(r'^user_lovecourse/$', user_lovecourse, name='user_lovecourse'),
+
+    url(r'^user_message/$', user_message, name='user_message'),
+    url(r'^user_deletemessage/$', user_deletemessage, name='user_deletemessage'),
+
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,20 +1,57 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
-from .forms import *
+from .forms import UserRegisterForm, UserLoginForm, UserForgetForm, UserResetForm, UserChangeImageForm, \
+    UserChangeEmailForm, UserChangeInfoForm, UserResetEmailForm
 from .models import UserProfile, EmailVerifyCode, BannerInfo
-from tools.send_mail_tool import send_email_code
 from django.db.models import Q
 from django.contrib.auth import authenticate, logout, login
-from django.core.mail import send_mail
+from tools.send_mail_tool import send_email_code
+from django.http import JsonResponse
+from datetime import datetime
+from operations.models import UserLove, UserMessage
+from orgs.models import OrgInfo, TeacherInfo
+from courses.models import CourseInfo
+from django.views.generic import View
+# Create your views here.
+from django.views.decorators.cache import cache_page
+
+# from redis import StrictRedis
 
 
+# class IndexView(View):
+#     def get(self,request):
+#         all_banners = BannerInfo.objects.all().order_by('-add_time')[:5]
+#         banner_courses = CourseInfo.objects.filter(is_banner=True)[:3]
+#         all_courses = CourseInfo.objects.filter(is_banner=False)[:6]
+#         all_orgs = OrgInfo.objects.all()[:15]
+#         return render(request, 'index.html', {
+#             'all_banners': all_banners,
+#             'banner_courses': banner_courses,
+#             'all_courses': all_courses,
+#             'all_orgs': all_orgs
+#         })
+
+
+@cache_page(15 * 60)
 def index(request):
-    return render(request, 'index.html')
+    all_banners = BannerInfo.objects.all().order_by('-add_time')[:5]
+    banner_courses = CourseInfo.objects.filter(is_banner=True)[:3]
+    all_courses = CourseInfo.objects.filter(is_banner=False)[:6]
+    all_orgs = OrgInfo.objects.all()[:15]
+    return render(request, 'index.html', {
+        'all_banners': all_banners,
+        'banner_courses': banner_courses,
+        'all_courses': all_courses,
+        'all_orgs': all_orgs
+    })
 
 
 def user_register(request):
     if request.method == 'GET':
+        # 这里实例化forms类，目的不是为了验证，而是为了使用验证码
         user_register_form = UserRegisterForm()
-        return render(request, 'users/register.html', {'user_register_form': user_register_form})
+        return render(request, 'users/register.html', {
+            'user_register_form': user_register_form
+        })
     else:
         user_register_form = UserRegisterForm(request.POST)
         if user_register_form.is_valid():
@@ -23,7 +60,6 @@ def user_register(request):
 
             user_list = UserProfile.objects.filter(Q(username=email) | Q(email=email))
             if user_list:
-                # return render(request, 'users/register.html', {
                 return render(request, 'users/register.html', {
                     'msg': '用户已经存在'
                 })
@@ -42,10 +78,11 @@ def user_register(request):
             })
 
 
-def user_login(request):
-    if request.method == 'GET':
+class UserLoginView(View):
+    def get(self, request):
         return render(request, 'users/login.html')
-    else:
+
+    def post(self, request):
         user_login_form = UserLoginForm(request.POST)
         if user_login_form.is_valid():
             username = user_login_form.cleaned_data['username']
@@ -55,18 +92,59 @@ def user_login(request):
             if user:
                 if user.is_start:
                     login(request, user)
-                    return redirect(reverse('index'))
+                    # 当登陆成功的时候,给加入一条消息
+                    a = UserMessage()
+                    a.message_man = user.id
+                    a.message_content = '欢迎登陆'
+                    a.save()
+                    url = request.COOKIES.get('url', '/')
+                    ret = redirect(url)
+                    ret.delete_cookie('url')
+                    return ret
                 else:
                     return HttpResponse('请去您的邮箱激活，否则无法登陆')
             else:
                 return render(request, 'users/login.html', {
-                    'msg': '邮箱或密码有误'
+                    'msg': '邮箱或者密码有误'
                 })
         else:
             return render(request, 'users/login.html', {
                 'user_login_form': user_login_form
             })
 
+
+# def user_login(request):
+#     if request.method == 'GET':
+#         return render(request,'users/login.html')
+#     else:
+#         user_login_form = UserLoginForm(request.POST)
+#         if user_login_form.is_valid():
+#             username = user_login_form.cleaned_data['username']
+#             password = user_login_form.cleaned_data['password']
+#
+#             user = authenticate(username=username,password=password)
+#             if user:
+#                 if user.is_start:
+#                     login(request,user)
+#                     #当登陆成功的时候,给加入一条消息
+#                     a = UserMessage()
+#                     a.message_man = user.id
+#                     a.message_content = '欢迎登陆'
+#                     a.save()
+#                     url = request.COOKIES.get('url','/')
+#                     ret = redirect(url)
+#                     ret.delete_cookie('url')
+#                     return ret
+#                 else:
+#                     return HttpResponse('请去您的邮箱激活，否则无法登陆')
+#             else:
+#                 return render(request,'users/login.html',{
+#                     'msg':'邮箱或者密码有误'
+#                 })
+#         else:
+#             return render(request, 'users/login.html', {
+#                 'user_login_form': user_login_form
+#             })
 
 def user_logout(request):
     logout(request)
@@ -154,3 +232,147 @@ def user_reset(request, code):
                     'user_reset_form': user_reset_form,
                     'code': code
                 })
+
+
+def user_info(request):
+    return render(request, 'users/usercenter-info.html')
+
+
+def user_changeimage(request):
+    # instance  指明实例是什么，做修改的时候，我们需要知道是给哪个对象实例进行修改
+    # 如果不指明，那么就会被当作创建对象去执行，而我们只有一个图片，就一定会报错。
+    user_changeimage_form = UserChangeImageForm(request.POST, request.FILES, instance=request.user)
+    if user_changeimage_form.is_valid():
+        user_changeimage_form.save(commit=True)
+        return JsonResponse({'status': 'ok'})
+    else:
+        return JsonResponse({'status': 'fail'})
+
+
+def user_changeinfo(request):
+    user_changeinfo_form = UserChangeInfoForm(request.POST, instance=request.user)
+    if user_changeinfo_form.is_valid():
+        user_changeinfo_form.save(commit=True)
+        return JsonResponse({"status": 'ok', 'msg': '修改成功'})
+    else:
+        return JsonResponse({"status": 'fail', 'msg': '修改失败'})
+
+
+def user_changeemail(request):
+    '''
+    当用户修改邮箱，点击获取验证码的时候，会通过这个函数处理，发送邮箱验证码
+    :param request: http请求对象
+    :return: 返回json数据，在模板页面当中去处理
+    '''
+    user_changeemail_form = UserChangeEmailForm(request.POST)
+    if user_changeemail_form.is_valid():
+        email = user_changeemail_form.cleaned_data['email']
+        user_list = UserProfile.objects.filter(Q(email=email) | Q(username=email))
+        if user_list:
+            return JsonResponse({'status': 'fail', 'msg': '邮箱已经被绑定'})
+        else:
+            # 我们在发送邮箱验证码之前，应该去邮箱验证码的表当中去查找，看之前有没有往当前这个邮箱发送过修改邮箱这个类型的验证码
+            email_ver_list = EmailVerifyCode.objects.filter(email=email, send_type=3)
+            # 如果发送过验证码，那么我们就拿到最近发送的这一个
+            if email_ver_list:
+                email_ver = email_ver_list.order_by('-add_time')[0]
+                # 判断当前时间和最近发送的验证码添加时间之差
+                if (datetime.now() - email_ver.add_time).seconds > 60:
+                    # 如果我们重新发送了新的验证码，那么以前最近发的就被清了
+                    send_email_code(email, 3)
+                    email_ver.delete()
+                    return JsonResponse({'status': 'ok', 'msg': '请尽快去邮箱当中获取验证码'})
+                else:
+                    return JsonResponse({'status': 'fail', 'msg': '请不要重复发送验证码，1分钟后重试'})
+            else:
+                send_email_code(email, 3)
+                return JsonResponse({'status': 'ok', 'msg': '请尽快去邮箱当中获取验证码'})
+    else:
+        return JsonResponse({'status': 'fail', 'msg': '您的邮箱有问题'})
+
+
+def user_resetemail(request):
+    user_resetemail_form = UserResetEmailForm(request.POST)
+    if user_resetemail_form.is_valid():
+        email = user_resetemail_form.cleaned_data['email']
+        code = user_resetemail_form.cleaned_data['code']
+        email_ver_list = EmailVerifyCode.objects.filter(email=email, code=code)
+        if email_ver_list:
+            email_ver = email_ver_list[0]
+            if (datetime.now() - email_ver.add_time).seconds < 60:
+                request.user.username = email
+                request.user.email = email
+                request.user.save()
+                return JsonResponse({'status': 'ok', 'msg': '邮箱修改成功'})
+            else:
+                return JsonResponse({'status': 'fail', 'msg': '验证码失效，请重新发送验证码'})
+        else:
+            return JsonResponse({'status': 'fail', 'msg': '邮箱或者验证码有误'})
+    else:
+        return JsonResponse({'status': 'fail', 'msg': '邮箱或者验证码不合法'})
+
+
+def user_course(request):
+    usercourse_list = request.user.usercourse_set.all()
+    course_list = [usercourse.study_course for usercourse in usercourse_list]
+    return render(request, 'users/usercenter-mycourse.html', {
+        'course_list': course_list
+    })
+
+
+def user_loveorg(request):
+    # userloveorg_list = request.user.userlove_set.all().filter(love_type=1)
+    userloveorg_list = UserLove.objects.filter(love_man=request.user, love_type=1, love_status=True)
+    org_ids = [userloveorg.love_id for userloveorg in userloveorg_list]
+    org_list = OrgInfo.objects.filter(id__in=org_ids)
+
+    return render(request, 'users/usercenter-fav-org.html', {
+        'org_list': org_list
+    })
+
+
+def user_loveteacher(request):
+    # userloveorg_list = request.user.userlove_set.all().filter(love_type=1)
+    userloveteacher_list = UserLove.objects.filter(love_man=request.user, love_type=3, love_status=True)
+    teacher_ids = [userloveteacher.love_id for userloveteacher in userloveteacher_list]
+    teacher_list = TeacherInfo.objects.filter(id__in=teacher_ids)
+
+    return render(request, 'users/usercenter-fav-teacher.html', {
+        'teacher_list': teacher_list
+    })
+
+
+def user_lovecourse(request):
+    userlovecourse_list = UserLove.objects.filter(love_man=request.user, love_type=2, love_status=True)
+    course_ids = [userlovecourse.love_id for userlovecourse in userlovecourse_list]
+    course_list = CourseInfo.objects.filter(id__in=course_ids)
+
+    return render(request, 'users/usercenter-fav-course.html', {
+        'course_list': course_list
+    })
+
+
+def user_message(request):
+    msg_list = UserMessage.objects.filter(message_man=request.user.id)
+    return render(request, 'users/usercenter-message.html', {
+        'msg_list': msg_list
+    })
+
+
+def user_deletemessage(request):
+    delete_id = request.GET.get('delete_id', '')
+    if delete_id:
+        msg = UserMessage.objects.filter(id=int(delete_id))[0]
+        msg.message_status = True
+        msg.save()
+        return JsonResponse({'status': 'ok', 'msg': '已读'})
+    else:
+        return JsonResponse({'status': 'fail', 'msg': '读取失败'})
+
+
+def handler_404(request):
+    return render(request, 'handler_404.html')
+
+
+def handler_500(request):
+    return render(request, 'handler_500.html')
